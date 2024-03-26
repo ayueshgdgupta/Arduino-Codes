f30eac4e2b17fb17635e9397720d66d81a9faeba
Ayuesh G <64496992+ayueshgdgupta@users.noreply.github.com>
2024-03-26 23:15:41 +0530

    Update Keypad.c

--- Keypad.c
+++ Keypad.c
@@ -1,20 +1,20 @@
 //To interface with 4 by 1 Keypad
 
-const int buttonPin[] = {7,6,5,4}; // the number of the pushbutton pins
-const int ledPin = 13; // the number of the LED pin
-// variables will change:
-int buttonState = 1; // variable for reading the pushbutton status
+const int buttonPin[] = {7,6,5,4}; 
+const int ledPin = 13; 
+
+int buttonState = 1; 
 void setup() {
 
- pinMode(ledPin, OUTPUT); // initialize the LED pin as an output:
- Serial.begin(9600); // initialize the Serial Monitor @ 9600
- for(int x=0; x<4; x++) // initialize the keypad pin(s) as an input:
+ pinMode(ledPin, OUTPUT); 
+ Serial.begin(9600); 
+ for(int x=0; x<4; x++) 
  pinMode(buttonPin[x], INPUT_PULLUP);
 
 }
 void loop(){
 
- for(int x=0; x<4; x++) // read the state of the keypad value:
+ for(int x=0; x<4; x++) 
  {
  buttonState = digitalRead(buttonPin[x]);
  if(buttonState==LOW)
